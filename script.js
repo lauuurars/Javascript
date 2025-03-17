4eaf68e6b2a37a6c98593df77692151c9ff2c8b4
Laura Sofia Riascos Burbano <1120098177@u.icesi.edu.co>
2025-03-17 09:42:58 -0500

    se realizó condicional

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
-let numero = 150;
+//let numero = 150;//
 
-console.log(numero); 
+//console.log(numero);// 
 
 //numero variable, let se usa para valores que pueden cambiar//
 
@@ -9,7 +9,73 @@ console.log(numero);
 
 //const se usa para valores que se mantienen iguales siempre, no cambian//
 
-let nombre = prompt('Ingresa tu nombre')
-console.log('Hola ' +  nombre);
+/*let nombre = prompt('Ingresa tu nombre')
+console.log('Hola ' +  nombre);*/
 
 //mensaje nombre en consola//
+
+// Son los tipos de datos más básicos que podemos encontrar en Javascript//
+
+/* let numero = 10; //number
+let texto = "Hola Mundo"; //String 
+let verdadero = true; //Boolean
+let falso = false; //Boolean
+let nodefenido; //Undefined
+let indefinido; //Undefined
+indefinido = "";
+let nulo = null; //Null
+
+console.log(typeof numero);
+console.log(typeof texto)
+console.log(typeof verdadero)
+console.log(typeof indefinido)
+console.log(nulo) */
+
+//Operadores aritméticos//
+
+/*let a = 20;
+let b = 5; 
+let suma = a + b;
+let resta = a -b; 
+let multiplica = a * b;
+let division = a / b; 
+let modulo = a % b;
+let exponente = a ** b;
+
+console.log(resta) //25
+console.log(suma)
+console.log(multiplica)
+console.log(division)
+console.log(modulo)
+console.log(exponente)*/
+
+/*let contador = 1;
+
+contador++
+;
+
+console.log(contador);*/
+
+//operadores de asignación//
+
+/* let a = 10; 
+let b = a + 5;
+
+a += 5;
+a -= 5;
+a *= 5;
+a /= 5;
+a %= 5;
+a **= 5;
+
+console.log(a);*/
+
+//Condicionales// 
+
+let numero = prompt("Ingresa un numero")
+if (numero >= 0){
+    alert("El numero es positivo");
+} else{
+    alert("El numero es negativo")
+}
+console.log("Siguiente flujo");
