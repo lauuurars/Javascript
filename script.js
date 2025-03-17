633bfaea3799a6878ebbe07ce9574cdc8821b752
Laura Sofia Riascos Burbano <1120098177@u.icesi.edu.co>
2025-03-17 08:22:52 -0500

    se agregaron apuntes

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 
+let numero = 150;
+
+console.log(numero); 
+
+//numero variable, let se usa para valores que pueden cambiar//
+
+//no usar var, usar let//
+
+//const se usa para valores que se mantienen iguales siempre, no cambian//
+
 let nombre = prompt('Ingresa tu nombre')
 console.log('Hola ' +  nombre);
 
