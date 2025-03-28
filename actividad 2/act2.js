34831cb11f649552fa36151acefe49d482277f28
lauuurars <1120098177@u.icesi.edu.co>
2025-03-28 17:39:04 -0500

    fin punto 2

---
+++ actividad 2/act2.js
@@ -0,0 +1,7 @@
+
+let numeros = [1, 2, 6, 3, 45, 0, 22, 7]
+
+let numerosPares = numeros.filter(numero => numero % 2 === 0) 
+
+console.log("original: " + numeros)
+console.log ("NUMEROS PARES: " + numerosPares)
